d9942b85324f17f4c91d94a1d06f754407700034
Diogo Silva <diogo@diogok.net>
2014-11-05 02:28:13 -0200

    initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Yet Another Augmented Reality in Javascript
+
+This is just a wrapper. Get the (rear, if available) camera into a canvas and detects augmented reality markers on it, smoothly and also works fine on mobile.
+
+[Just check it out](http://diogok.net/yaar).
+
+[js-aruco](https://code.google.com/p/js-aruco/) is a great augmented reality lib in javascript, based on [ArUco](http://www.uco.es/investiga/grupos/ava/node/26), a lightweight RA based on OpenCV.
+
+Also checkout the [js aruco markers](http://github.com/diogok/js-aruco-markers) to use with this.
+
+## Compatibility
+
+Work and tested on:
+
+- Desktop Firefox
+- Desktop Chrome
+- Android Firefox
+- Android Chrome
+
+Does Not Work on:
+
+- Android Stock Browser
+- iOS Safari
+- Desktop Safari
+- IE
+
+Likely, but not tested:
+
+- iOS Chrome
+- iOS Firefox
+
+Does not work on Android nor iOS stock browser so, no phonegap or cordova. But could work on [Chrome Apps](https://github.com/MobileChromeApps/mobile-chrome-apps), got to test that.
+
+## Usage
+
+Place yarr.js and detector.js in known place. Detector actually contains the code for OpenCV and Aruco in it, and it works as a webworker.
+
+Take a look at index.html.
+
+So, simply:
+
+  var opts = {
+    id: 'canvas-id', // canvas for the yaar to use
+    draw: true, // If I should draw lines on the found markers, good for debug
+    onMarker: function(markers){} // listen for found markers, with their ids and position
+  };
+  var instance = yaar(opts);
+  // sometime later
+  instance.stop();
+
+
+## License
+
+OpenCV is BSD.
+
+JS-Aruco is MIT.
+
+This is MIT.
+
---
+++ yaar.js
@@ -0,0 +1,111 @@
+
+navigator.getMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia); 
+
+function yaar(opts) {
+
+  var width  = 640;
+  var height = 480;
+
+  var video = document.createElement('video');
+  video.setAttribute('id','yarr-video');
+  video.setAttribute('autoplay','autoplay');
+  video.style.display='none';
+  document.body.appendChild(video);
+
+  var canvas = document.getElementById(opts.id);
+  //canvas.style.display='none';
+  var context = canvas.getContext('2d');
+
+  var path = (opts.detector?opts.detector:'detector.js');
+
+  var worker = new Worker(path);
+  var imageData = null;
+
+  var entities=[];
+
+  worker.addEventListener('message',function(e){
+      entities=[];
+      for(var i=0;i<e.data.length;i++) {
+        entities.push(e.data[i]);
+        if(typeof opts.onMarker == 'function') {
+          opts.onMarker(e.data[i]);
+        }
+      }
+  });
+
+  function update() {
+    worker.postMessage(imageData);
+  }
+
+  function render() {
+    animation = requestAnimationFrame(render);
+
+    try {
+      context.drawImage(video,0,0,width,height);
+      imageData = context.getImageData(0,0,width,height);
+    } catch(ex) {
+    }
+
+    if(typeof opts.draw != 'undefined' && opts.draw === true) {
+      var markers = entities.slice();
+
+      for(var i=0;i<markers.length;i++) {
+        context.strokeStyle='#FF0000';
+        context.fillStyle='#FF0000';
+        context.beginPath();
+        context.moveTo(markers[i].corners[0].x, markers[i].corners[0].y);
+        context.lineTo(markers[i].corners[1].x, markers[i].corners[1].y);
+        context.lineTo(markers[i].corners[2].x, markers[i].corners[2].y);
+        context.lineTo(markers[i].corners[3].x, markers[i].corners[3].y);
+        context.lineTo(markers[i].corners[0].x, markers[i].corners[0].y);
+        context.stroke();
+        context.font='30px Verdana';
+        context.fillText(markers[i].id,markers[i].corners[0].x + 10, markers[i].corners[0].y + 10 );
+      }
+    }
+
+  };
+
+  function startCapture() {
+    function start(cfg) {
+      navigator.getMedia(cfg, success, error);
+      function error(e) {
+        console.log('error',e);
+      }
+      function success(stream) {
+        video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+        video.play();
+        setTimeout(function(){
+          height = video.videoHeight / (video.videoWidth/width);
+          video.setAttribute('width', width);
+          video.setAttribute('height', height);
+          canvas.setAttribute('width', width);
+          canvas.setAttribute('height', height);
+        },1000);
+      }
+    }
+    if(MediaStreamTrack.getSources) {
+      MediaStreamTrack.getSources(function(sources){
+        var sid = sources[sources.length - 1].id;
+        start({video: {optional:[{sourceId: sid}]}, audio: false})
+      });
+    } else {
+      start({video:true,audio:false});
+    }
+  };
+
+  startCapture();
+
+  var animation = requestAnimationFrame(render);
+  var interval  = setInterval(update,1000/6);
+
+  return {
+    stop: function() {
+      clearInterval(interval);
+      cancelAnimationFrame(animation);
+      document.body.removeChild(video);
+    }
+  };
+
+}
+
